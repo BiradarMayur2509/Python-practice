a20ebd1e76b5d4b011341dd568d3f2655b2f5727
Biradar Mayur Mahadev <biradarmayur2509@gmail.com>
2024-11-21 15:09:50 +0530

    first commit

---
+++ While_loop/1.py
@@ -0,0 +1,7 @@
+# Example of using while loops in Python
+
+N = int(input())
+count = 0
+while count <= N:
+    print(count)
+    count += 1
---
+++ While_loop/2.py
@@ -0,0 +1,11 @@
+#Example of using the break statement in while loops
+
+N = int(input())
+count = 0
+
+while count <= N:
+    print("Hello Mayur")
+    count += 1
+    
+    if count == 4:
+        break
