ae2d5c1c98261ebafc2fa1c3293fbb4a40852484
Biradar Mayur Mahadev <biradarmayur2509@gmail.com>
2024-11-23 12:19:04 +0530

    New

---
+++ While_loop/print_N_integers.py
@@ -0,0 +1,10 @@
+# Write a program to print integers from 1 to the given integer(N)
+
+# output should be of N lines,printing an integer in each lines.
+
+N = int(input())
+count = 1
+
+while count <= N:
+    print(count)
+    count = count + 1
---
+++ While_loop/print_N_time.py
@@ -0,0 +1,8 @@
+# Write a program that reads number N and prints number the number 0, N times on N lines.
+
+N = int(input())
+count = 0
+
+while count < N:
+    print("0")
+    count = count + 1
